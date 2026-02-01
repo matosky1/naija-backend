45f8c7c225b26b6112b9944794af7ba57d931016
Anthony <odinukweanthony@gmail.com>
2026-02-01 02:29:38 -0700

    updated my server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.post("/api/square/charge", async (req, res) => {
         const nodemailer = require("nodemailer");
         const transporter = nodemailer.createTransport({
           host: process.env.EMAIL_HOST,
-          port: Number(process.env.EMAIL_PORT || 587),
+          port: Number(process.env.EMAIL_PORT || 465),
           secure: false,
           auth: {
             user: process.env.EMAIL_USER,
